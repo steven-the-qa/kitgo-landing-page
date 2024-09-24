bd32ebf44695bc86b06fa9386718d10ad3185611
Steven Boutcher <boutchersj@gmail.com>
2024-09-24 13:48:11 -0500

    First commit: set up Astro landing page template for KitGo using v0 and Cursor

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
