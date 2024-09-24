1db40d9d943df4832cb473166fff53f13f6361d8
Steven Boutcher <boutchersj@gmail.com>
2024-09-24 14:47:41 -0500

    Prepare for Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
+import tailwind from '@astrojs/tailwind';
+
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
